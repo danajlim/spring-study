6d68e87b65ea7c569c5f01c0ddc8efb4895b81b2
danajlim <danalim0819@gmail.com>
2025-04-08 14:48:07 +0900

    ResponseBody 객체 반환 함수

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,5 +24,34 @@ public class HelloController {
         //name이라는 이름으로 템플릿에 데이터를 넘김
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    //ResponseBody 문자 반환
+    @GetMapping("hello-string")
+    @ResponseBody //반환값을 응답 body에 그대로 넣어서 보냄
+    public String helloString(@RequestParam("name")  String name){
+    return "hello " + name; //"hello spring"
+    }
+
+    //예 : http://localhost:8080/hello-api?name=Dana
+    //ResponseBody 객체 반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    //(@RequestParam("name") String name) : ?name=xxx 읽어와서 name 변수에 담음
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //빈 객체 생성
+        hello.setName(name); //전달받은 이름으로
+        return hello; //응답으로 반환
+    }
+
+    static class Hello {
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+
     }
 }
