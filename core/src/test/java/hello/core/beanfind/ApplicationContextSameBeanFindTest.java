a0dc66166a4e1972a688be1e47647980d9949859
danajlim <danalim0819@gmail.com>
2025-04-14 11:55:17 +0900

    스프링 빈 조회 - 상속 관계

--- core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -21,32 +21,41 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ApplicationContextSameBeanFindTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
+    //타입으로만 조회할때 예외 발생
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생")
     void findBeanByTypeDuplicate(){
+        //어떤 빈을 줘야할지 모호해서 NoUniqueBeanDefinitionException 예외 발생함
         assertThrows(NoUniqueBeanDefinitionException.class,
                 () -> ac.getBean(MemberRepository.class));
     }
 
+    //이름과 타입으로 조회
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면 빈 이름을 지정하면 됨")
     void findBeanByName(){
+        //구체적인 이름 memberRepository1을 지정해줌 -> 정상적으로 동작
         MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
 
+    //특정 타입의 모든 빈 조회
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
     void findAllBeanType(){
+        //해당 타입 모든 빈을 Map으로 반환 <빈 이름 : 빈 객체>
         Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + "value = " + beansOfType.get(key));
         }
         System.out.println("beansOfType = " + beansOfType);
+        //수가 2개인지 검증
         assertThat(beansOfType.size()).isEqualTo(2);
     }
 
+    //내부 설정 클래스
     @Configuration
+    //스프링 컨테이너에 MemberRepository 타입의 빈 두개 있음
     static class SameBeanConfig {
         @Bean
         public MemberRepository memberRepository1() {
