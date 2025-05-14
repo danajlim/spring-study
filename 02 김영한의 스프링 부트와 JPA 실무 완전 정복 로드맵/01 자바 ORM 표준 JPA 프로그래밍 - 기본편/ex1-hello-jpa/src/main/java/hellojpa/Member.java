ad7a80426f38d3bcc57226529577bddd208e6c94
danajlim <danalim0819@gmail.com>
2025-05-14 15:40:42 +0900

    필드와 컬럼 매핑

--- 02 김영한의 스프링 부트와 JPA 실무 완전 정복 로드맵/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 02 김영한의 스프링 부트와 JPA 실무 완전 정복 로드맵/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,25 +1,48 @@
 package hellojpa;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
+@Entity //JPA에서 관리할 엔티티다
 public class Member {
-    public Member(String id, String name) {
+
+    @Id //이 필드가 기본키를 인식
+    private Long id;
+
+    /* name -> 실제 필드명과 DB 컬럼명이 다를 때 지정
+    * nullable = false -> NOT NULL 조건
+    * length = 10 -> 문자 길이 제약 조건, String에서만
+    * columnDefinition = "varchar(100) Default 'EMPTY'" -> 컬럼 정보를 직접 넣어줌
+    * */
+    @Column(name = "name", nullable = false, length = 50, columnDefinition = "varchar(100) default ‘EMPTY'")
+    private String name;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) //enum을 DB에 문자열로 안전하게 저장
+    private RoleType roleType;
+
+    private LocalDateTime createdDate; //LocalDateTime: date + time
+    private LocalDateTime lastModifiedDate;
+
+    @Lob //대용량 데이터 저장을 위한 애노테이션
+    private String description;
+
+    //Constructor
+    public Member() {
+    }
+    public Member(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public Member() {
-    }
-
-    @Id
-    private String id;
-    private String name;
-
-    public String getId() {
+    //Getter, Setter
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
---
+++ 02 김영한의 스프링 부트와 JPA 실무 완전 정복 로드맵/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/RoleType.java
@@ -0,0 +1,6 @@
+package hellojpa;
+
+//enum 타입: 열거형 상수로 미리 정의된 값만 사용할 수 있게 제한하는 자바 문법
+public enum RoleType {
+    USER, ADMIN
+}
