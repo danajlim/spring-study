29d5a3ca47a2c281ce23fe3d6a32536a795aba46
danajlim <danalim0819@gmail.com>
2025-05-14 15:54:20 +0900

    sequence 전략

--- 02 김영한의 스프링 부트와 JPA 실무 완전 정복 로드맵/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 02 김영한의 스프링 부트와 JPA 실무 완전 정복 로드맵/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,39 +5,23 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity //JPA에서 관리할 엔티티다
+@SequenceGenerator(name = "member_seq_generator", //JPA가 사용할 시퀀스(DB 자동 증가 번호)를 설정해주는 애노테이션
+        sequenceName = "member_seq",
+        initialValue = 1000,
+        allocationSize = 5)
 public class Member {
 
     @Id //이 필드가 기본키를 인식
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "member_seq_generator") //JPA에서 기본 키 값을 자동 생성해주는 방법
     private Long id;
 
-    /* name -> 실제 필드명과 DB 컬럼명이 다를 때 지정
-    * nullable = false -> NOT NULL 조건
-    * length = 10 -> 문자 길이 제약 조건, String에서만
-    * columnDefinition = "varchar(100) Default 'EMPTY'" -> 컬럼 정보를 직접 넣어줌
-    * */
     @Column(name = "name", nullable = false, length = 50, columnDefinition = "varchar(100) default ‘EMPTY'")
     private String name;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING) //enum을 DB에 문자열로 안전하게 저장
-    private RoleType roleType;
-
-    private LocalDateTime createdDate; //LocalDateTime: date + time
-    private LocalDateTime lastModifiedDate;
-
-    @Lob //대용량 데이터 저장을 위한 애노테이션
-    private String description;
 
     //Constructor
     public Member() {
     }
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    //Getter, Setter
     public Long getId() {
         return id;
     }
