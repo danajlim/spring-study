b98eeee347c2112aeeebbca601a46090e63a3c24
danajlim <danalim0819@gmail.com>
2025-05-15 10:12:33 +0900

    fix: files

--- 02 SpringBoot:JPA/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 02 SpringBoot:JPA/01 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -5,14 +5,16 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity //JPA에서 관리할 엔티티다
-@SequenceGenerator(name = "member_seq_generator", //JPA가 사용할 시퀀스(DB 자동 증가 번호)를 설정해주는 애노테이션
-        sequenceName = "member_seq",
-        initialValue = 1000,
-        allocationSize = 5)
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ",
+        allocationSize = 1
+)
 public class Member {
 
     @Id //이 필드가 기본키를 인식
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "member_seq_generator") //JPA에서 기본 키 값을 자동 생성해주는 방법
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR") //JPA에서 기본 키 값을 자동 생성해주는 방법
     private Long id;
 
     @Column(name = "name", nullable = false, length = 50, columnDefinition = "varchar(100) default ‘EMPTY'")
